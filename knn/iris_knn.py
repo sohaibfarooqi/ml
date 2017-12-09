62ebbada1de4caab5946003e097402b98fdac007
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2017-12-10 00:48:05 +0800

    knn impl for iris dataset

---
+++ knn/iris_knn.py
@@ -0,0 +1,34 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+from util import plot_decision_regions
+
+iris = datasets.load_iris()
+x = iris.data[:, [2,3]]
+y = iris.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+sc.fit(x_train)
+x_train_std = sc.transform(x_train)
+x_test_std = sc.transform(x_test)
+
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(x_train_std, y_train)
+
+x_combined_std = np.vstack((x_train_std, x_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(x_combined_std, y_combined, knn)
+
+plt.show()
+
