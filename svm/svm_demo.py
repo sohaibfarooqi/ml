7904e13ae1980fa92243179750284b19c1654c6c
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2017-12-09 20:08:25 +0800

    svm with random points

---
+++ svm/svm_demo.py
@@ -0,0 +1,27 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.svm import SVC
+from util import plot_decision_regions
+
+np.random.seed(0)
+
+x_xor = np.random.randn(200,2)
+y_xor = np.logical_xor(x_xor[:, 0] > 0, x_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+# plt.scatter(x_xor[y_xor == 1, 0], x_xor[y_xor==1, 1], c='b', marker='x', label='1')
+# plt.scatter(x_xor[y_xor == -1, 0], x_xor[y_xor== -1, 1], c='r', marker='s', label='-1')
+
+# plt.ylim(-3, 0)
+# plt.legend()
+# plt.show()
+
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10.0)
+svm.fit(x_xor, y_xor)
+
+plot_decision_regions(x_xor, y_xor, classifier=svm)
+plt.show()
