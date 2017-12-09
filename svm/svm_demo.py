3ab869143292b9fc5288edc82c39a67ca6d6a061
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2017-12-09 20:09:07 +0800

    svm with random points added scatter plot

--- svm/svm_demo.py
+++ svm/svm_demo.py
@@ -12,13 +12,11 @@ x_xor = np.random.randn(200,2)
 y_xor = np.logical_xor(x_xor[:, 0] > 0, x_xor[:, 1] > 0)
 y_xor = np.where(y_xor, 1, -1)
 
-# plt.scatter(x_xor[y_xor == 1, 0], x_xor[y_xor==1, 1], c='b', marker='x', label='1')
-# plt.scatter(x_xor[y_xor == -1, 0], x_xor[y_xor== -1, 1], c='r', marker='s', label='-1')
+plt.scatter(x_xor[y_xor == 1, 0], x_xor[y_xor==1, 1], c='b', marker='x', label='1')
+plt.scatter(x_xor[y_xor == -1, 0], x_xor[y_xor== -1, 1], c='r', marker='s', label='-1')
 
-# plt.ylim(-3, 0)
-# plt.legend()
-# plt.show()
-
+plt.ylim(-3, 0)
+plt.legend()
 
 svm = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10.0)
 svm.fit(x_xor, y_xor)
